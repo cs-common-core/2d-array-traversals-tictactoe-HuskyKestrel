c6d9475f6275ae44368273143d7c507fea3e50a0
Rylan Singh <rsingh25@nobles.edu>
2025-02-13 16:44:48 +0000

    feb 13th

--- src/Play.java
+++ src/Play.java
@@ -1,15 +1,38 @@
 import java.util.Scanner;
 public class Play {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Tic Tac Toe!");
         System.out.println("Player 1 is X, Player 2 is O");
-        System.out.println("Enter the row and column to play");
         TicTacToe game = new TicTacToe();
+        int player = 1;
+        int row = 0;
+        int colum = 0;
+        boolean played;
+
         while(!game.isGameOver()) {
-            game.printBoard();
+
+            System.out.println("Player " + player + " enter your row");
+            row = input.nextInt() - 1;
+            System.out.println("Player " + player + " enter your colum");
+            colum = input.nextInt() - 1;
+
+            played = game.play(row, colum);
+
             // your code here
+
+            if (player == 1 && played)
+            {
+                player = 2;
+            }
+            else if (played) {
+                player = 1;
+            }
+            
+            game.printBoard();
         }
-        scanner.close();
+        System.out.println("Player " + game.getWinner() + " won.");
+        input.close();
+
     }
 }
